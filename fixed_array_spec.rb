069d3fae95d208cf71283df6490ba40afdf000ea
emritcey <emritcey@gmail.com>
2016-08-22 16:19:26 -0500

    write initial tests for #new, both fail

--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,16 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
+	describe "#new" do 
+		it "has a default size of 20" do 
+			array = FixedArray.new 
+			expect(array.size).to eq 20
+		end 
+
+		it "takes a variable which defines the size" do 
+			array = FixedArray.new(25)
+			expect(array.size).to eq 25
+		end 
+	end 
   
 end
